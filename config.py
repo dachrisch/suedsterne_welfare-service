22f12d0c2dae883afa4375bc4110c45a20ee0a6c
Christian Dähn <christian.daehn@gmail.com>
2018-11-27 20:17:34 +0100

    main app

--- config.py
+++ config.py
@@ -2,7 +2,6 @@
 
 MUCKI_TRACKER_SHEET_ID = '1x5TECCvP3sF3cmMJiD5frkjkmGOVt2pWgNe2eB2zZtI'
 MUCKI_TRACKER_TEAM_STATUS_RANGE = 'status!A2:D11'
-MEINE_WOCHE_GROUP_ID = 207628
 APPLICATION_NAME = 'Muckibot'
 CLIENT_SECRET_FILE = 'client_secret.json'
 SCOPES = ('https://www.googleapis.com/auth/spreadsheets.readonly',)
